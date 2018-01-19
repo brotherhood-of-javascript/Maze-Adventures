317f0a2c0c9e7dfb4e8f71fd1b381520174e562d
okyesanap <okyesanap@gmail.com>
2018-01-19 11:22:30 +0200

    ver0.10

---
+++ src/components/DevelopersScreen.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="section background-dark">
+      <div class="container text-center">
+        <h3 class="text-huge text-white text-with-subtitle">DEVELOPERS</h3>
+        <h4 class="text-big text-gray">Nadia Bilozerova</h4>
+        <h4 class="text-big text-gray">Andrii Marchenko</h4>
+        <h4 class="text-big text-gray">Andrii Panaseiko</h4>
+      </div>
+    </div>
+</template>
+<script>
+export default { name: 'DevelopersScreen' }
+</script>
