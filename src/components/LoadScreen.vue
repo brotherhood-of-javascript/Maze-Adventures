edd08679a8cdf7dfb4e14a572452617e8101cbc1
andrii.p.marchenko <marchenko1993.a@gmail.com>
2018-01-23 19:09:08 +0200

    changed functional for save game and added functional for rewrite save, fixed problems with remove

--- src/components/LoadScreen.vue
+++ src/components/LoadScreen.vue
@@ -1,130 +1,86 @@
 <template>
   <section class="section container">
     <h2 class="text-gray title-save-load-menu">Load Game</h2>
+    <div class="col-xs-12 ">
       <table class="table save-table">
-         <thead>
-           <tr class="text-medium align-center">
-             <th>Name Loading</th>
-             <th>Date</th>
-             <th>Remove</th>
-             </tr>
-         </thead>
-      <tbody @click="chooseLoader">
-        <tr v-for="item in keyNames" class="keyName">
-          <td>{{ item }}</td>
-          <td>{{ date }}</td>
-          <td><button 
-                class="button button-red button-small block-mobile remove-btn"
-                @click="getRemove"
-              >remove</button>
-          </td> 
+        <thead>
+        <tr class="text-medium align-center">
+          <th>Name Loading</th>
+          <th>Date</th>
+          <th>Remove</th>
         </tr>
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+        <tr v-for='names in saveObjectsArray' @click='takeСurrentName(names.id)'>
+          <td>{{names.id}}</td>
+          <td>{{names.date}} </td>
+          <td><button class="button button-red button-small block-mobile remove-btn" @click="remove(names.id)">remove</button></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
     <div class="col-xs-12">
-      <button
-        class="button button-primary button-big block-mobile save-btn"
-        @click="getLoad"
-      >Load</button>
-    </div>  
+      <button class="button button-primary button-big block-mobile save-btn " @click="Load">Load</button>
+    </div>
   </section>
 </template>
 <script>
-export default {
-  name: 'LoadScreen',
-  data: function() {
-    return {
-      key: '',
-      date: '13.01.2018'
-    }
-  },
-  methods: {
-    chooseLoader() {
-      let target = (event && event.target) || event.srcElement
-      if (target.parentNode.classList.contains('keyName')) {
-        let boxContainer = target.parentNode.parentNode.children
-
-        for (let i = 0; i < boxContainer.length; i++) {
-          if (boxContainer[i].classList.contains('choose')) {
-            boxContainer[i].classList.remove('choose')
-          }
-        }
-        target.parentNode.classList.add('choose')
+  export default {
+    name: 'LoadScreen',
+    data: function() {
+      return {
+        key: '',
+        date: '13.01.2018',
+        saveObjectsArray: [],
+        currentName: ''
       }
     },
-    getLoad(event) {
-      let key = ''
-      if (event.target.tagName.toLowerCase() === 'button') {
-        let boxContainer = document.querySelector('tbody').children
-
-        for (let i = 0; i < boxContainer.length; i++) {
-          if (boxContainer[i].classList.contains('choose')) {
-            key = boxContainer[i].firstElementChild.innerText
+    methods: {
+      takeСurrentName(names) {
+        this.currentName = names
+      },
+      Load() {
+        for (var i = 0; i < this.saveObjectsArray.length; i++) {
+          if (this.saveObjectsArray[i].id === this.currentName) {
+            this.$store.commit('loadNewState', {key: this.$store.getters.globalKey, value: this.saveObjectsArray})
+            this.$router.push({ name: 'GameScreen' })
           }
         }
-        this.$store.commit('createNewState', key)
-        this.$router.push({ name: 'GameScreen' })
+      },
+      remove(names) {
+        for (var i = 0; i < this.saveObjectsArray.length; i++) {
+          if (this.saveObjectsArray[i].id === names) {
+            this.saveObjectsArray.splice(i, 1)
+            this.$store.commit('saveNewState', {key: this.$store.getters.globalKey, value: this.saveObjectsArray})
+            break
+          }
+        }
       }
     },
-    getRemove(event) {
-      let boxAll = document.querySelector('tbody')
-      let boxContainer = event.target.parentNode.parentNode
-      let key = boxContainer.firstElementChild.innerText
-
-      localStorage.removeItem(key)
-      boxAll.removeChild(boxContainer)
+    created() {
+      let saveArray = localStorage.getItem(this.$store.getters.globalKey)
+      if (saveArray) {
+        this.saveObjectsArray = JSON.parse(saveArray)
+      }
     }
-  },
-  beforeCreate() {
-    let arrKey = []
-    for (var i = 0; i < localStorage.length; i++) {
-      arrKey.push(localStorage.key(i))
-    }
-    arrKey.shift()
-    this.keyNames = arrKey
-  },
-  created() {}
-}
+  }
 </script>
 
 <style scoped>
-.title-save-load-menu {
-  font-size: 50px;
-  padding: 30px 0 40px 0;
-}
-.save-btn {
-  padding: 10px 80px;
-}
-.table > tbody > tr:hover {
-  color: #1abc9c;
-}
-.input-save-group {
-  padding-bottom: 40px;
-}
-.save-table {
-  font-size: 20px;
-}
-.input {
-  display: inline-block;
-  margin-left: 0;
-  margin-right: 0;
-  width: 100%;
-  border: 0;
-  font-size: 20px;
-  box-shadow: none;
-  color: #fff;
-  height: 82px;
-  line-height: 44px;
-  margin-bottom: 0;
-  outline: none;
-  padding-left: 10px;
-  padding-right: 10px;
-  width: 100%;
-}
-.input::-webkit-input-placeholder {
-  color: #fff;
-}
-.choose {
-  background: #547aa0;
-}
+  .title-save-load-menu {
+    font-size: 50px;
+    padding: 30px 0 40px 0;
+  }
+  .save-btn {
+    padding: 10px 80px;
+  }
+  .table>tbody>tr:hover {
+    background: #547aa0
+  }
+  .input-save-group{
+    padding-bottom: 40px;
+  }
+  .save-table {
+    font-size: 20px;
+  }
 </style>
