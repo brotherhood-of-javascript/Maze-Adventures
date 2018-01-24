ee22985d37f607b661b679e98aa0be6e8fa60399
okyesanap <okyesanap@gmail.com>
2018-01-24 11:11:13 +0200

    loadsAndSavesWorking

--- src/components/LoadScreen.vue
+++ src/components/LoadScreen.vue
@@ -1,27 +1,32 @@
 <template>
   <section class="section container">
     <h2 class="text-gray title-save-load-menu">Load Game</h2>
-    <div class="col-xs-12 ">
       <table class="table save-table">
-        <thead>
-        <tr class="text-medium align-center">
-          <th>Name Loading</th>
-          <th>Date</th>
-          <th>Remove</th>
+         <thead>
+           <tr class="text-medium align-center">
+             <th>Name Loading</th>
+             <th>Date</th>
+             <th>Remove</th>
+             </tr>
+         </thead>
+      <tbody>
+        <tr v-for="load in allSaves" class="keyName" @click="choseSave(load.state, load.id)">
+          <td> {{load.id}}</td>
+          <td> {{load.date}}</td>
+          <td><button 
+                class="button button-red button-small block-mobile remove-btn"
+               
+              >remove</button>
+          </td> 
         </tr>
-        </thead>
-        <tbody>
-        <tr v-for='names in saveObjectsArray' @click='takeСurrentName(names.id)'>
-          <td>{{names.id}}</td>
-          <td>{{names.date}} </td>
-          <td><button class="button button-red button-small block-mobile remove-btn" @click="remove(names.id)">remove</button></td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
+      </tbody>
+    </table>
+    <div class="text-white text-medium" v-show="currentName!=''">Game to load {{currentName}}</div>
     <div class="col-xs-12">
-      <button class="button button-primary button-big block-mobile save-btn " @click="Load">Load</button>
-    </div>
+      <button
+        class="button button-primary button-big block-mobile save-btn" @click="loadGame"
+      >Load</button>
+    </div>  
   </section>
 </template>
 <script>
@@ -29,38 +34,19 @@ export default {
   name: 'LoadScreen',
   data: function() {
     return {
-      key: '',
-      date: '13.01.2018',
-      saveObjectsArray: [],
+      allSaves: JSON.parse(localStorage.saveKey),
+      currentSave: {},
       currentName: ''
     }
   },
   methods: {
-    takeСurrentName(names) {
-      this.currentName = names
+    choseSave: function(curLoad, curName) {
+      this.currentSave = curLoad
+      this.currentName = curName
     },
-    Load() {
-      for (var i = 0; i < this.saveObjectsArray.length; i++) {
-        if (this.saveObjectsArray[i].id === this.currentName) {
-          this.$store.commit('loadNewState', { key: this.$store.getters.globalKey, value: this.saveObjectsArray })
-          this.$router.push({ name: 'GameScreen' })
-        }
-      }
-    },
-    remove(names) {
-      for (var i = 0; i < this.saveObjectsArray.length; i++) {
-        if (this.saveObjectsArray[i].id === names) {
-          this.saveObjectsArray.splice(i, 1)
-          this.$store.commit('saveNewState', { key: this.$store.getters.globalKey, value: this.saveObjectsArray })
-          break
-        }
-      }
-    }
-  },
-  created() {
-    let saveArray = localStorage.getItem(this.$store.getters.globalKey)
-    if (saveArray) {
-      this.saveObjectsArray = JSON.parse(saveArray)
+    loadGame: function() {
+      this.$store.commit('loaderGame', { loadedGame: this.currentSave })
+      this.$router.push({ name: 'GameScreen' })
     }
   }
 }
@@ -75,12 +61,35 @@ export default {
   padding: 10px 80px;
 }
 .table > tbody > tr:hover {
-  background: #547aa0;
+  color: #1abc9c;
 }
 .input-save-group {
   padding-bottom: 40px;
 }
 .save-table {
   font-size: 20px;
+}
+.input {
+  display: inline-block;
+  margin-left: 0;
+  margin-right: 0;
+  width: 100%;
+  border: 0;
+  font-size: 20px;
+  box-shadow: none;
+  color: #fff;
+  height: 82px;
+  line-height: 44px;
+  margin-bottom: 0;
+  outline: none;
+  padding-left: 10px;
+  padding-right: 10px;
+  width: 100%;
+}
+.input::-webkit-input-placeholder {
+  color: #fff;
+}
+.choose {
+  background: #547aa0;
 }
 </style>
